482a928059d068855a402ffd9cb0823180a8169c
Giacomo Schmidt Frattari <au447879@uni.au.dk>
2026-01-23 15:59:16 +0100

    Adapted docker structure setup to work even if data has not been downloaded

--- single_cell_scripts/R/utilities/structure_setup.R
+++ single_cell_scripts/R/utilities/structure_setup.R
@@ -17,34 +17,39 @@ print("Organizing matrix data")
 dir.create("data/raw_data/single_cell_data/sc_data_by_donor",
            recursive = T)
 
-# Move single cell data by donor under raw data
-file.rename("data/sc_data_by_donor", 
-            "data/raw_data/single_cell_data/sc_data_by_donor")
-
-# Split into by-sample directories
-# Identify sample names
-list.files("data/raw_data/single_cell_data/sc_data_by_donor") %>%
-  sub("(aim_ID....*_v...{0,1})_.+", "\\1", .) %>%
-  unique() %>%
-  # For each sample name
-  for(sample in .){
-    
-    # Create a directory
-    dir.create(paste0(raw_data, sample))
-    
-    # Move filtered matrix
-    file.rename(paste0(raw_data, sample, "_sample_filtered_feature_bc_matrix.h5"), 
-                paste0(raw_data, "/", sample, "/sample_filtered_feature_bc_matrix.h5"))
-    
-    # Move raw matrix
-    file.rename(paste0(raw_data, sample, "_raw_feature_bc_matrix.h5"), 
-                paste0(raw_data, "/", sample, "/raw_feature_bc_matrix.h5"))
-    
-    # Move tcr annotations
-    file.rename(paste0(raw_data, sample, "_filtered_contig_annotations.csv"), 
-                paste0(raw_data, "/", sample, "/filtered_contig_annotations.csv"))
-    
-  }
+# If data is available
+if(file.exists("data/sc_data_by_donor")){
+  
+  # Move single cell data by donor under raw data
+  file.rename("data/sc_data_by_donor", 
+              "data/raw_data/single_cell_data/sc_data_by_donor") 
+  
+  # Split into by-sample directories
+  # Identify sample names
+  list.files("data/raw_data/single_cell_data/sc_data_by_donor") %>%
+    sub("(aim_ID....*_v...{0,1})_.+", "\\1", .) %>%
+    unique() %>%
+    # For each sample name
+    for(sample in .){
+      
+      # Create a directory
+      dir.create(paste0(raw_data, sample))
+      
+      # Move filtered matrix
+      file.rename(paste0(raw_data, sample, "_sample_filtered_feature_bc_matrix.h5"), 
+                  paste0(raw_data, "/", sample, "/sample_filtered_feature_bc_matrix.h5"))
+      
+      # Move raw matrix
+      file.rename(paste0(raw_data, sample, "_raw_feature_bc_matrix.h5"), 
+                  paste0(raw_data, "/", sample, "/raw_feature_bc_matrix.h5"))
+      
+      # Move tcr annotations
+      file.rename(paste0(raw_data, sample, "_filtered_contig_annotations.csv"), 
+                  paste0(raw_data, "/", sample, "/filtered_contig_annotations.csv"))
+      
+    }
+  
+}
 
 # --- 1. Create processed_data directory ---
 
@@ -58,9 +63,8 @@ dir.create("data/processed_data/")
 dir.create("data/processed_data/steps/")
 
 # Directories with shared names between steps and objects
-common.dir <- c("adt", "dab", "deg", "objects", "qc")
-
-sapply(common.dir, function(x) dir.create(paste0(processed_data, "steps/", x)))
+sapply(c("adt", "dab", "deg", "objects", "qc"), 
+       function(x) dir.create(paste0(processed_data, "steps/", x)))
 
 # Steps-specific directories
 dir.create(paste0(processed_data, "steps/adt/dsb"))
@@ -72,7 +76,8 @@ dir.create(paste0(processed_data, "steps/tcr"))
 dir.create("data/processed_data/results/")
 
 # Directories with shared names between steps and results
-sapply(common.dir, function(x) dir.create(paste0(processed_data, "results/", x)))
+sapply(c("adt", "dab", "deg", "objects", "qc"), 
+       function(x) dir.create(paste0(processed_data, "results/", x)))
 
 # Result-specific directories
 dir.create(paste0(processed_data, "results/figures"))
